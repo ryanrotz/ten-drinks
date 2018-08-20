df1911c11be2d30f8f2e0fac9c66aa504f92f2f3
Ryan Rotz <rrotz@tirebuyer.com>
2018-08-19 18:07:45 -0700

    styled home page into big tiles

--- src/app/drinks-lists.service.ts
+++ src/app/drinks-lists.service.ts
@@ -9,22 +9,41 @@ export class DrinksListsService {
 	drinksLists: DrinksListsModel[] = [
 		{
 			id: 1,
-			title: 'Cocktails to order at ANY bar',
+			title: 'Dive bar',
 			shortDesc: 'At a dive with no menu? Order smart and quick!',
 			view: '/dive-bar'
 		},
 		{
 			id: 2,
-			title: 'Cocktails to order at a fancy bar',
+			title: 'Fancy bar',
 			shortDesc: 'Consider these your must try cocktails..',
 			view: '/fancy-bar'
 		},
 		{
 			id: 3,
-			title: 'Mocktails to enjoy',
+			title: 'Mocktails',
 			shortDesc: 'What to order sans booze.',
 			view: '/mocktails'
-		}
+		},
+		{
+			id: 4,
+			title: 'Gin',
+			shortDesc: 'What to order sans booze.',
+			view: '/'
+		},
+		{
+			id: 6,
+			title: 'Tiki',
+			shortDesc: 'What to order sans booze.',
+			view: '/'
+		},
+		{
+			id: 7,
+			title: 'Whiskey',
+			shortDesc: 'What to order sans booze.',
+			view: '/'
+		},
+
 	]
 
 
